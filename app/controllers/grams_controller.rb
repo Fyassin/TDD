20d804ac8aec852e1383d86fed0fbeb73bdeed4d
Faneeza Yassin <faneeza.yassin@gmail.com>
2019-09-08 16:12:58 +0000

    lesson 12 complete, 12 passing test

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,26 +2,34 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
 	def new
-    	@gram = Gram.new
-  	end
+    @gram = Gram.new
+  end
 
 	def index
-  	end
+  end
 
-  	def create
-  		@gram = current_user.grams.create(gram_params)
-  		if @gram.valid?
-        redirect_to root_path
-      else
-        render :new, status: :unprocessable_entity
-      end
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
 
-  	end
 
-  	private
+  def create
+  	@gram = current_user.grams.create(gram_params)
+  	if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
 
-  	def gram_params
-    	params.require(:gram).permit(:message)
-  	end
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 
 end
